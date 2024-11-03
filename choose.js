a7f27339186fd236a409afc1dfdd2f46b92aa483
PoanTra <githubwork0@gmail.com>
2024-11-03 20:23:18 +0700

    Add files via upload

---
+++ choose.js
@@ -0,0 +1,8 @@
+function showSidebar(){
+    const showSidebar = document.querySelector('.sidebar')
+    showSidebar.style.display = 'flex'
+}
+function hideSibar(){
+    const hideSibar = document.querySelector('.sidebar')
+    hideSibar.style.display = 'none'
+}
